b9e53364f6e684291079c832227e0fc2fd790f8d
aaln <aaron@thecompassmag.com>
2013-07-22 03:14:41 -0400

    added sockets and fixed lots of bugs

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
+var fs = require('fs');
 var path = require('path');
 var mongoose = require('mongoose');
+var mime  = require('mime');
+var io = require('socket.io');
+var parseCookie = require('connect').utils.parseCookie;
 var app = express();
+var server = http.createServer(app);
 
 mongoose.connect('mongodb://localhost/node');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
 
-var app = express();
+var db = mongoose.connection;
 
 app.engine('html', require('ejs').renderFile);
 
@@ -23,7 +25,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser('secret-code'));
-  app.use(express.session({secret:'hello'}));
+  app.use(express.session({secret: 'secret', key: 'express.sid'}));
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
@@ -33,90 +35,80 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-// initialize schema 
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-	firstName: { type: String, required: true },
-	lastName: { type: String, required: true },
-	email: { type: String, required: true },
-	username: { type: String, required: true },
-	password: String}, 
-    { collection : 'user' });
+var testSchema = new Schema({
+	firstName: String,
+	lastName: String
+}, {collection: 'test' });
 
-var User = mongoose.model('User', userSchema);
+var Test = mongoose.model('Test', testSchema);
 
 app.get('/', function(req, res){
-      res.render('index', {
-            title: 'Home',
-        });
-
+	res.render('chat', {sid:'hey'})
 });
-
-app.get('/signup', function(req, res) {
-	res.render('signup', {
-		title: 'Signup'
+app.post('/', function(req, res) {
+	var user = new Test();
+	user.firstName = req.param('firstName');
+	user.lastName = req.param('lastName');
+	user.save(function(err) {
+		if (err)
+			console.log('meow')
+	});
+	res.render('index',{
+		title: userData.first
+		})
+});
+app.get('/chat', function(req, res) {
+	res.render('chat');
+});
+app.get('/find', function(req, res) {
+	res.render('find', {
+		title: userData
 	})
+	});
+
+app.get('/spec', function(req, res) {
+	res.render('spectator');
 });
 
-app.post('/signup', function(req, res){
-
-	if(signup(req.body)) {
-		var user = new User();
-		user.firstName = req.param('firstName');
-		user.lastName = req.param('lastName');
-		user.email = req.param('email');
-		user.username = req.param('username');
-		user.password = req.param('password');
-		user.save(function (err) {
-  		if (err) // ...
-  		console.log('meow');
-	});
-	res.render('index', {
-		title : user.firstname
-	});
-	req.session.email = user.email;
-}
-	else {
-		res.render('index', {
-			title : 'Sorry, all fields are required'
-		});
-	}
+app.get('/agenda', function(req, res) {
+	res.render('agenda');
 });
+server.listen(3000);
 
-function signup(form) {
-	if(form.username.length > 0 && form.email.length > 0){
-		if(validateEmail(form.email)){
-			if(findone('email', form.email) === null) {
-				if(findone('username', form.username) === null) {
-					return true;
+var io = require('socket.io').listen(server);
 
-			
-				}	
-			}
-		}
-	}
+usernames = {};
+var chatData = [];
+io.sockets.on('connection', function (socket) {
+	for(var i = 0; i < chatData.length; i = i + 2) {
+    socket.emit('updatechat', chatData[i], chatData[i+1])
+  }
 
-	else {
-		return false;
-	}
-}
-
-function findone(key, value) {
-	user.findOne({key: value}, function(err, obj) {
-		if (err) throw err;
-		return obj;
+	socket.on('sendchat', function (data) {
+		chatData.push(socket.username);
+    	chatData.push(data);
+		io.sockets.emit('updatechat', socket.username, data)
 	});
-}
 
-function validateEmail(email) { 
-  // http://stackoverflow.com/a/46181/11236
-  // http://jsbin.com/ozeyag/800/edit
-    var re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
-    return re.test(email);
-}
+	socket.on('adduser', function(username) {
+		socket.username = username;
+		usernames[username] = username;
+		socket.emit('updatechat', 'SERVER', 'you have connected');
+		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
 
+		io.sockets.emit('updateusers', usernames)
+	});
 
+	socket.on('disconnect', function() {
+		delete usernames[socket.username];
+		io.sockets.emit('updateusers', usernames)
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+	});
 
-app.listen(3000);
-console.log('Server running on 3000.')
+	socket.on('newAgenda', function(itemId) {
+		socket.broadcast.emit('updateAgenda', itemId)
+	});
+	console.log('A socket is connected!')
+});
