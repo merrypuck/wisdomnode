f33a901abcb6d3c56d8e4aeb40c7e0a0d805056a
aaln <aaron@thecompassmag.com>
2013-07-29 22:36:10 -0400

    adding chat module

--- config/oauth_providers.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  twit: {
-    consumerKey: 'JLCGyLzuOK1BjnKPKGyQ',
-    consumerSecret: 'GNqKfPqtzOcsCtFbGTMqinoATHvBcy1nzCTimeA9M0'
-  },
-};
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var mime  = require('mime');
 var io = require('socket.io');
 var parseCookie = require('connect').utils.parseCookie;
 var flash = require('connect-flash');
+var groupchat = require('./lib/groupchat')
 var app = express();
 var server = http.createServer(app);
 
@@ -51,118 +52,29 @@ var userSchema= new Schema({
 }, {collection: 'user' });
 
 var User = mongoose.model('User', userSchema);
-
+var io = require('socket.io').listen(server);
 app.get('/', function(req, res){
-	res.render('index', {title:'a'})
+	res.render('login', {
+
+	})
 });
 
 app.post('/', function(req, res) {
-	if(User.findOne({'username':req.param('username')}) != null) {
-		if(User.findOne({'email':req.param('email')}) != null) {
-			var user = new User();
-			user.firstName = req.param('firstName');
-			user.lastName = req.param('lastName');
-			user.username = req.param('username');
-			user.email = req.param('email');
-			user.organization = req.param('organization');
-			user.orgtitle = req.param('title');
-			user.save(function(err) {
-			if (err)
-				console.log('meow')
+	var thisSession = wGroupChat.newChatroom(req.param('sessionNum'));
+	thisSession.init(io, 'blankForNow', req.param('sessionNum'));
+
+	io.sockets.on('connection', function(socket) {
+		thisSession.joinChatroom(req.param('username'), socket);
+		socket.emit('connectionAlert', 'connected!');
 	});
-	res.render('chat',{
-		firstName : user.firstName,
-		title: req.session.user
-		});
-	res.cookie('remember', '1', { maxAge: 120000 })
-	req.session.username = user.username;
-	
 
-		}
-		else {
-			res.render('signup', {
-			title : 'Sorry, your email has already been used.'
+	res.render('expert',{
+		username : req.param('username'),
+		//title: req.session.user
 		});
-		}
-	}
-
-	else {
-		res.render('signup', {
-			title : 'Sorry, your username is already taken'
-		});
-	}
+	//res.cookie('remember', '1', { maxAge: 120000 })
+	//req.session.username = user.username;
 });
-app.get('/expert', function(req, res) {
-	res.render('expert')
-});
-app.get('/ej', function(req, res) {
-	res.render('spectator')
-});
-function restrict(req, res, next) {
-	if(req.session.user) {
-		next();
-	}
-	else {
-		res.render('signup', {
-			title:'Sorry, you must be signed in to see this page.'
-			
-		});
-	}
-}
-
-app.get('/chat', restrict, function(req, res) {
-	res.render('chat', {title: req.session.user});
-});
-
 
 
 server.listen(4000);
-
-var io = require('socket.io').listen(server);
-
-usernames = {};
-notes = {};
-var chatData = [];
-var rooms = ['room1', 'room2', 'room3']
-io.sockets.on('connection', function (socket) {
-	for(var i = 0; i < chatData.length; i = i + 2) {
-    socket.emit('updatechat', chatData[i], chatData[i+1])
-  }
-
-	socket.on('sendchat', function (data) {
-		chatData.push(socket.username);
-    	chatData.push(data);
-		io.sockets.emit('updatechat', socket.username, data)
-	});
-
-	socket.on('adduser', function(username) {
-		socket.username = username;
-		usernames[username] = username;
-		socket.emit('updatechat', 'SERVER', 'you have connected');
-		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-
-		io.sockets.emit('updateusers', usernames)
-	});
-
-	socket.on('disconnect', function() {
-		delete usernames[socket.username];
-		io.sockets.emit('updateusers', usernames)
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-	});
-	socket.on('newAgenda', function(agendaItem) {
-		console.log('reached server.')
-		socket.emit('updateAgenda', agendaItem)
-		socket.broadcast.emit('updateAgenda', agendaItem)
-	});
-	
-	socket.on('editAgenda', function(item, data) {
-		socket.emit('editTopic', item, data)
-	});
-
-	socket.on('saveNotes', function(notes) {
-		socket.emit('timestampNote', notes)
-	});
-	socket.on('editTopic', function() {
-
-	})
-}); 
