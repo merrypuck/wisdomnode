eee0a663b7a6296140c02082a275864812866506
aaln <aaron@thecompassmag.com>
2013-08-13 08:01:10 -0400

    new changes for wastewise

--- server.js
+++ server.js
@@ -19,7 +19,6 @@
 
 var express = require('express');
 var http = require('http');
-var https = require('https');
 var fs = require('fs');
 var path = require('path');
 var crypto = require('crypto');
@@ -46,26 +45,18 @@ var LocalStrategy = require('passport-local').Strategy;
 
 // Configuration settings.
 var app = express();
-
-var options = {
-	key: fs.readFileSync('livewisdomlykey.key'),
-	cert: fs.readFileSync('livewisdomly.crt'),
-	ca: fs.readFileSync('livewisdomlyca.pem')
-}
-
 var server = http.createServer(app);
 
 mongoose.connect('mongodb://localhost/wisdom1');
 var db = mongoose.connection;
 var fileServer = new nodestatic.Server(__dirname + '/public/static');
 
-var port =8002;
-var serverAddr = "http://127.0.0.1"
+
 
 app.engine('html', require('ejs').renderFile);
 
 app.configure(function(){
-  app.set('port', process.env.PORT || port);
+  app.set('port', process.env.PORT || 8002);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'html');
   app.set('view options', {layout: false});
@@ -108,7 +99,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new LinkedInStrategy({
     consumerKey: LINKEDIN_API_KEY,
     consumerSecret: LINKEDIN_SECRET_KEY,
-    callbackURL: serverAddr + ":" + port+ "/auth/linkedin/callback",
+    callbackURL: "http://localhost:8002/auth/linkedin/callback",
     profileFields: ['id', 'first-name', 'last-name', 'email-address', 'headline','picture-url'] 
   },
   function(token, tokenSecret, profile, done) {
@@ -205,31 +196,6 @@ var userSchema = new Schema({
 	password : { type: String, required: true },
 }, {collection : 'users1'});
 
-// Bcrypt middleware
-userSchema.pre('save', function(next) {
-	var user = this;
-
-	if(!user.isModified('password')) return next();
-
-	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-		if(err) return next(err);
-
-		bcrypt.hash(user.password, salt, function(err, hash) {
-			if(err) return next(err);
-			user.password = hash;
-			next();
-		});
-	});
-});
-
-// Password verification
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-		if(err) return cb(err);
-		cb(null, isMatch);
-	});
-};
-
 // TODO(mj): Move this to another file that would contain all the schemas.
 /*var userSchema = new Schema({
 	firstName : String,
@@ -314,7 +280,6 @@ var dummyTalk = {
 			});*/
 		
 app.get('/expert1', function(req, res) {
-	console.log(JSON.stringify(req.user));
 	if(req.user.provider === 'twitter') {
 	userCred = {
 		userId : req.user.id,
@@ -350,10 +315,10 @@ app.get('/expert1', function(req, res) {
 			console.log('error');
 		}
 	});
+	}
 	res.render('expert1', {
 		user: userCred
 	});
-}
 });
 
 app.get('/expert', function(req, res) {
@@ -369,36 +334,24 @@ app.get('/login', function(req, res) {
 	});
 });
 
-app.get('/moderator', function(req, res){
-
-	res.render('moderator',{
-		user: "kevin@wisdom.ly"
-	});
-
-});
-
-app.get('/signup', function(req, res) {
-	res.render('login')
-});
 app.post('/signup', function(req, res) {
 	userCred = {
-			firstName : req.body.firstName,
-			lastName : req.body.lastName,
+			userId : req.body.email,
+			nickname : req.body.nickname,
+			firstName : req.body.nickname,
+			lastName : '',
 			email : req.body.email,
 			profilePic : 'http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-2.jpg',
-			password : req.body.password
 		};
 		var user = new User1();
-		user.firstName = userCred.firstName;
-		user.lastName = userCred.lastName;
+		user.nickname = userCred.nickname;
 		user.email = userCred.email;
-		user.password = userCred.password;
 		user.save(function(err) {
 		if (err){
 			console.log('error');
 		}
 	});
-	
+
     res.render('expert1', {
     	user : userCred
     });
@@ -626,4 +579,4 @@ wtwitter.init(io,
 
 	});
 
-server.listen(port);
+server.listen(8002);
